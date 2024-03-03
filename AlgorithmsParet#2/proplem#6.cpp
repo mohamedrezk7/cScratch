50d60ea3061eb0e2578f0525747cad851a01f040
MohamedRezk <mohamedrezk7@gmail.com>
2024-03-03 20:53:35 +0200

    lastUpdate

--- AlgorithmsParet#2/proplem#6.cpp
+++ AlgorithmsParet#2/proplem#6.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cmath>
 using namespace std;
-// write a programe to read a number from user and print it reversed order 
+// write a programe to read a number from user and print the of its digits 
 enum EnPerfectNumber {perfect = 1 , notperfect =2 };
 float  ReadPositiveNumber (string message){
     int number = 0 ;
@@ -13,13 +13,15 @@ float  ReadPositiveNumber (string message){
     return number ;
 }
 void PrintReversedGigit (int number){
-    int reminder = 0 ;
+    int reminder = 0 , sumdigits = 0 ;
+
     while (number > 0 ){
         reminder = number%10;
         number = number /10 ;
-        cout<<reminder<<endl;
+        (sumdigits+=reminder);
         // cout <<"infinity";
     };
+    cout << " sum OF Digits is = " <<sumdigits<<endl;
 }
 int main (){
 PrintReversedGigit(ReadPositiveNumber("pleas enter positive number"));
